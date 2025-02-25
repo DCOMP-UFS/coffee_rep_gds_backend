ba784ce24a2742c65ed288070ef0875f1c051967
Raí Rafael <rairafaelss@gmail.com>
2025-02-25 00:12:53 -0300

    Fix lint

--- src/main/java/br/ufs/coffee_rep_gds_backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/ufs/coffee_rep_gds_backend/exceptions/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
