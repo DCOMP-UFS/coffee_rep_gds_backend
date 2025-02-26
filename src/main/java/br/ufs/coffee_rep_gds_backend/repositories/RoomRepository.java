74b91be8d0ba3360746feef996855be5f37a48c7
Raí Rafael <rairafaelss@gmail.com>
2025-02-26 11:41:27 -0300

    Add unpaged to Room Get Request

--- src/main/java/br/ufs/coffee_rep_gds_backend/repositories/RoomRepository.java
+++ src/main/java/br/ufs/coffee_rep_gds_backend/repositories/RoomRepository.java
@@ -8,25 +8,27 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT distinct r.id, r.name, tt.name as type, ts.name as section, case when tr.id is not null then true else false end as ocupationStatus " +
-                                       "FROM tb_rooms r " +
-                                       "left join tb_reservations tr on r.id = tr.room_id " +
-                                       "and now() >= tr.start_date and now() <= tr.end_date  and tr.status = 1 " +
-                                       "left join tb_sections ts on r.section_id = ts.id " +
-                                       "left join tb_room_types tt on r.room_type_id = tt.id " +
-                                       "where r.status = :status " +
-                                       "and r.id = :id " +
-                                       "and (r.name like concat('%', :name, '%') or :name is null) " +
-                                       "and (tt.name like concat('%', :type, '%') or :type is null) " +
-                                       "and (ts.name like concat('%', :section, '%') or :section is null) " +
-                                       "and (:ocupationStatus is null or " +
-                                       "(:ocupationStatus = true and tr.id is not null) or " +
-                                       "(:ocupationStatus = false AND tr.id IS NULL))")
+    String query = "SELECT distinct r.id, r.name, tt.name as type, ts.name as section, case when tr.id is not null then true else false end as ocupationStatus " +
+                   "FROM tb_rooms r " +
+                   "left join tb_reservations tr on r.id = tr.room_id " +
+                   "and now() >= tr.start_date and now() <= tr.end_date  and tr.status = 1 " +
+                   "left join tb_sections ts on r.section_id = ts.id " +
+                   "left join tb_room_types tt on r.room_type_id = tt.id " +
+                   "where r.status = :status " +
+                   "and (r.name like concat('%', :name, '%') or :name is null) " +
+                   "and (tt.name like concat('%', :type, '%') or :type is null) " +
+                   "and (ts.name like concat('%', :section, '%') or :section is null) " +
+                   "and (:ocupationStatus is null or " +
+                   "(:ocupationStatus = true and tr.id is not null) or " +
+                   "(:ocupationStatus = false AND tr.id IS NULL))";
+
+    @Query(nativeQuery = true, value = query)
     Optional<RoomProjection> findActive(Long id, Integer status, String name, String type, Boolean ocupationStatus, String section);
 
     Optional<Room> findByIdAndStatus(Long id, Integer status);
@@ -46,20 +48,11 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
                                        "(:ocupationStatus = false AND tr.id IS NULL))")
     Page<RoomProjection> findBySectionId(Long sectionId, Integer status, String name, String type, Boolean ocupationStatus, Pageable pageable);
 
-    @Query(nativeQuery = true, value = "SELECT distinct r.id, r.name, tt.name as type, ts.name as section, case when tr.id is not null then true else false end as ocupationStatus " +
-                                       "FROM tb_rooms r " +
-                                       "left join tb_reservations tr on r.id = tr.room_id " +
-                                       "and now() >= tr.start_date and now() <= tr.end_date  and tr.status = 1 " +
-                                       "left join tb_sections ts on r.section_id = ts.id " +
-                                       "left join tb_room_types tt on r.room_type_id = tt.id " +
-                                       "where r.status = :status " +
-                                       "and (r.name like concat('%', :name, '%') or :name is null) " +
-                                       "and (tt.name like concat('%', :type, '%') or :type is null) " +
-                                       "and (ts.name like concat('%', :section, '%') or :section is null) " +
-                                       "and (:ocupationStatus is null or " +
-                                       "(:ocupationStatus = true and tr.id is not null) or " +
-                                       "(:ocupationStatus = false AND tr.id IS NULL))")
+    @Query(nativeQuery = true, value = query)
     Page<RoomProjection> findRoomWithOccupation(Integer status, String name, String type, String section, Boolean ocupationStatus, Pageable pageable);
 
     Optional<Room> getRoomByNameIgnoreCaseAndSection(String name, Section section);
+
+    @Query(nativeQuery = true, value = query)
+    List<RoomProjection> findAllActiveRoomUnpaged(Integer status, String name, String type, String section, Boolean ocupationStatus);
 }
