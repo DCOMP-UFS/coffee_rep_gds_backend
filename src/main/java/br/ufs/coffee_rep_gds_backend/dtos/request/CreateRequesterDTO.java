ca70aa7cd7e3c1f6e757682055600f7fb310043c
Guilherme Rosário Alves <guilhermera272@gmail.com>
2026-05-24 14:49:46 -0300

    refactor(requester): atualizar DTOs sem campo cpf
    Remove CPF dos DTOs de criação, atualização e resposta de solicitante.
    Elimina validações @CPF e @NotEmpty relacionadas ao documento no cadastro.

--- src/main/java/br/ufs/coffee_rep_gds_backend/dtos/request/CreateRequesterDTO.java
+++ src/main/java/br/ufs/coffee_rep_gds_backend/dtos/request/CreateRequesterDTO.java
@@ -2,16 +2,11 @@ package br.ufs.coffee_rep_gds_backend.dtos.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import org.hibernate.validator.constraints.br.CPF;
 
 public record CreateRequesterDTO(
         @NotEmpty(message = "O nome do solicitante deve ser preenchido!")
         @NotBlank(message = "O nome do solicitante não deve ser deixado em branco!")
         String nome,
-
-        @CPF(message = "Por favor, insira um CPF no formato correto!")
-        @NotEmpty(message = "O CPF deve ser preenchido!")
-        String cpf,
 
         String telefone,
 
--- src/main/java/br/ufs/coffee_rep_gds_backend/dtos/request/UpdateRequesterDTO.java
+++ src/main/java/br/ufs/coffee_rep_gds_backend/dtos/request/UpdateRequesterDTO.java
@@ -1,12 +1,7 @@
 package br.ufs.coffee_rep_gds_backend.dtos.request;
 
-import org.hibernate.validator.constraints.br.CPF;
-
 public record UpdateRequesterDTO(
         String nome,
-
-        @CPF(message = "Por favor, insira um CPF válido!")
-        String cpf,
         String telefone,
         String especialidade
 ) {
